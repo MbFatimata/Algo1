23f72cd37602ff2e6af41ad10d6826c94fc1aded
thomaslegrand4815162342 <tlegrand@orange.fr>
2018-05-31 15:34:33 +0200

    affiche menu

--- Algo1/Program.cs
+++ Algo1/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algo1
+namespace Menu
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+
+        }
+        private static int AfficherMenu(List<string> menu)
+        {
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+
+            }
+
+            Console.Write("Faites votre choix");
+            int ChoixUtilisateur = int.Parse(Console.ReadLine());
+            return ChoixUtilisateur;
+
+
         }
     }
 }
