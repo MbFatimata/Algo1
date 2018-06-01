a7d3c80aecd5e707ef808a42bd79014fddd90b7a
thomaslegrand4815162342 <tlegrand@orange.fr>
2018-06-01 11:58:29 +0200

    partie2bis correction

--- Algo1/Program.cs
+++ Algo1/Program.cs
@@ -4,28 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Menu
+namespace Algo1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            List<MenuItem> list = new List<MenuItem>
+            {
+                new MenuItem { Numero =1, Libelle= "menu 1"},
+                new MenuItem { Numero =2, Libelle= "menu 2"},
+                new MenuItem { Numero =3, Libelle= "menu 3"},
+                new MenuItem { Numero =4, Libelle= "menu 4"},
+                new MenuItem { Numero =5, Libelle= "menu 5"},
+                new MenuItem { Numero =5, Libelle= "menu 5"},
+            };
+            int resultat = GererMenu(list);
+            Console.WriteLine(resultat);
+            Console.ReadLine();
 
         }
-        private static int AfficherMenu(List<string> menu)
+        private static int GererMenu(List<MenuItem> menu)
         {
-            foreach (string ligne in menu)
+            //Afiche les elements du menu
+            foreach (MenuItem ligne in menu)
             {
-                Console.WriteLine(ligne);
+                Console.WriteLine($"{ligne.Numero} - {ligne.Libelle}");
 
             }
 
-            Console.Write("Faites votre choix");
+            // recuperation choix utilisateur
+            Console.WriteLine("Faites votre choix ");
             int ChoixUtilisateur = int.Parse(Console.ReadLine());
-            return ChoixUtilisateur;
 
+            //test du choix par rapport aux elements du menu
+            while (true)
+            {
+                foreach (MenuItem ligne in menu)
+                {
+                    if (ChoixUtilisateur == ligne.Numero)
+                    {
+                        return ChoixUtilisateur;
+                    }
+                    //return menu.SingleOrDefault(x=> x.Numero == choixUtilisateur) != null ? choix : -1;              
+                }
+                Console.WriteLine("Erreur lors du choix");
+            }
+        }
 
+        /// <summary>
+        /// MenuItem représente une ligne du menu
+        /// </summary>
+        class MenuItem
+        {
+            public int Numero { get; set; }
+            public string Libelle { get; set; }
+        }
+        class NumeroItem
+        {
+            public int Numero { get; set; }
         }
     }
 }
+
+            
+
